2ee72b389f0d160f44890372d16bb0e82da8ca47
Christian Sachs <byaskew3@gmail.com>
2024-11-20 17:52:02 -0700

    Fetching Data with Async/Await

--- Fetching Data and Handling Promises with Callbacks/main.js
+++ Fetching Data and Handling Promises with Callbacks/main.js
@@ -6,8 +6,13 @@
 
 // caching the dom
 const pokeForm = document.querySelector('form')
-const pokeCardImage = document.querySelector('img')
-const pokeCardName = document.querySelector('h5')
+const userPokeImg = document.querySelector('#userPokeImg')
+const userPokeName = document.querySelector('#userPokeName')
+const userPokeCard = document.querySelector('#userPokeCard')
+const enemyPokeImg = document.querySelector('#enemyPokeImg')
+const enemyPokeName = document.querySelector('#enemyPokeName')
+const enemyPokeCard = document.querySelector('#enemyPokeCard')
+const btnContainer = document.querySelector('#btn-container')
 
 // fetch to pokemon api using fetch API and .then(), .catch() callbacks
 const getPokemon = (pokeName) => {
@@ -16,8 +21,11 @@ const getPokemon = (pokeName) => {
         return response.json()
     })
     .then((data) => {
-        pokeCardImage.src = data.sprites.front_default
-        pokeCardName.textContent = data.name
+        userPokeImg.src = data.sprites.front_default
+        userPokeName.textContent = data.name
+        // showing card when data is retrieved
+        userPokeCard.style.display = ''
+        createBattleBtn()
     })
     // .catch() only runs on errors
     .catch((error) => {
@@ -29,5 +37,34 @@ pokeForm.addEventListener('submit', (event) => {
     const pokeInput = document.querySelector('#pokeInput').value
     getPokemon(pokeInput)
 })
+
+// create battle button function
+const createBattleBtn = () => {
+    const battleBtn = document.createElement('button')
+    battleBtn.classList = 'btn btn-danger'
+    battleBtn.textContent = 'Battle'
+    battleBtn.id = 'battle-btn'
+    if(!document.querySelector('#battle-btn')){
+        btnContainer.append(battleBtn)
+        battleBtn.addEventListener('click', getEnemyPoke)
+    }
+}
+
+// create async function getEnemyPokemon
+const getEnemyPoke = async () => {
+    const randomPoke = Math.floor(Math.random() * 152)
+    console.log(randomPoke)
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPoke}`)
+        const data = await response.json()
+        // manipulating dom with enemy pokemon data
+        enemyPokeImg.src = data.sprites.front_default
+        enemyPokeName.textContent = data.name
+        // showing card when data is retrieved
+        enemyPokeCard.style.display = ''
+    } catch(err) {
+        console.log(err)
+    }
+}
 
 
